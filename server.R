45420c62b6ca1bc204eea2f91742311a61b0083b
Kevin Kane <kevin@kevinkane.org>
2017-06-19 17:04:16 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -1,43 +1,33 @@
-setwd("E:/Documents/MFI/webmap/relocations")
 library(shiny)
 library(leaflet)
 library(sp)
 library(maptools)
 
-# Reads in shapefiles, generates a data frame version of it to use for data display 'dfsub' and converts zero values to NA
 sub <- readShapePoly("SoCal_place_2010_all")
 dfsub <- data.frame(sub)
-fips = unique(unlist(dfsub$plfips))
-
+cats <- read.csv("SoCal_place_2010_allcats.csv")
 
 shinyServer(function(input, output) {
-  
-  # (1) REACTIVE FUNCTION FOR ZOOM-TO-CITY. Default coords are Irvine.
+  #### (1) REACTIVE FUNCTION FOR ZOOM-TO-CITY. Default coordinates are Irvine.
   center <- reactiveValues(x_coord=-117.7736, y_coord=33.67801)
   observeEvent(input$go, {
     center$x_coord = as.numeric(as.character(dfsub$INTPTLON10[dfsub$NAME10==input$city]))
     center$y_coord = as.numeric(as.character(dfsub$INTPTLAT10[dfsub$NAME10==input$city]))
   })
-  
-  # (2) REACTIVE FUNCTION FOR GRABBING USER INPUT AS 'options$choose'
+  #### (2) REACTIVE FUNCTION FOR GRABBING USER INPUT AS 'options$choose'
   options = reactiveValues(choose="iall36770")
   observeEvent(input$go, {
-    ind_link = switch(input$industry, "All"="all", "Retail"="ret", "Education/Healthcare"="hlt",
-                      "FIRE"="fir", "Professional"="prf", "Service"="svc", "Industrial"="ind")
     tofromlink = switch(input$inout, "relocating to"="i", "relocating from"="o")
     cityfips = dfsub$plfips[dfsub$NAME10==input$city]
-    options$choose = paste(tofromlink, ind_link, cityfips, sep="")
+    options$choose = paste(tofromlink, "all", cityfips, sep="")
   })
-  
-  # (3) MAKE THE MAP AS A REACTIVE TO USER INPUTS
+  #### (3) MAKE THE MAP AS A REACTIVE TO USER INPUTS
   finalMap <- reactive ({
     withProgress(message='Please Wait: Map Loading', {
     choice = dfsub[,grep(options$choose, colnames(dfsub))]
     choice2 = choice
     choice[choice==0] <- NA
     pal <- colorBin("YlOrRd", choice, bins=c(1, 5, 10, 25, 50, 100, 25000), na.color="#FFFFFF")
-    
-    
     # Create map. Invoke leaflet command on data object 'sub' to create map object 'm'
     m = leaflet(sub) %>%  setView(lng=center$x_coord, lat=center$y_coord, zoom=11) %>% addTiles() %>%
       addPolygons(data=sub, stroke=T, weight=1.1, fillColor = ~pal(choice), color="black", fillOpacity=0.5, 
@@ -48,24 +38,46 @@ shinyServer(function(input, output) {
     })
   })
   
-  # (4) PASS THE REACTIVE MAP TO AN OUTPUT OBJECT CALLED 'output$myMap' 
+  #### (4) PASS THE REACTIVE MAP TO AN OUTPUT OBJECT CALLED 'output$myMap' 
   output$myMap = renderLeaflet(finalMap())
-
-  # (5) TEXT OUTPUT
-  output$within <- renderText({
-    paste("Total businesses relocating WITHIN ", input$city, ":", dfsub[,grep(options$choose, colnames(dfsub))][dfsub$NAME10==input$city])
-  })
-  output$total <- renderText({
-    paste("Total businesses ", input$inout, input$city, ":", sum(dfsub[,grep(options$choose, colnames(dfsub))][dfsub$NAME10!=input$city]))
-  })
-  output$tablelabel <- renderText({
-    paste("Top 5 cities which businesses", input$inout, input$city, switch(input$inout, "relocating to"="come from:", "relocating from"="go to:"))
-  })
+  
+  #### (5) TEXT OUTPUT - MAP PAGE
+  output$within <- renderText(paste("Total businesses relocating WITHIN ", input$city, ":", dfsub[,grep(options$choose, colnames(dfsub))][dfsub$NAME10==input$city]))
+  output$total <- renderText(paste("Total businesses ", input$inout, input$city, ":", sum(dfsub[,grep(options$choose, colnames(dfsub))][dfsub$NAME10!=input$city])))
+  output$tablelabel <- renderText(paste("Top 5 cities which businesses", input$inout, input$city, switch(input$inout, "relocating to"="come from:", "relocating from"="go to:")))
   output$topfive <- renderTable({
-    a <- cbind(
-      as.character(dfsub[order(-dfsub[,grep(options$choose, colnames(dfsub))]),][2:6,5]),
+    a <- cbind(as.character(dfsub[order(-dfsub[,grep(options$choose, colnames(dfsub))]),][2:6,5]),
       as.numeric(dfsub[order(-dfsub[,grep(options$choose, colnames(dfsub))]),][2:6,grep(options$choose, colnames(dfsub))])  )
   })
   
+  #### (6) REACTIVE SELECTION - MULTIPLE INDUSTRIES PAGE 
+  options2 = reactiveValues(choose2="iall36770")
+  observeEvent(input$go2, {
+    ind_link2 = switch(input$industry2, "All"="all", "Retail"="ret", "Education/Healthcare"="hlt",
+                      "FIRE"="fir", "Professional"="prf", "Service"="svc", "Industrial"="ind")
+    tofromlink2 = switch(input$inout2, "relocating to"="i", "relocating from"="o")
+    cityfips2 = cats$plfips[cats$NAME10==input$city2]
+    options2$choose2 = paste(tofromlink2, ind_link2, cityfips2, sep="")
+  })
+  
+  #### (7) TEXT OUTPUT - MULTIPLE INDUSTRIES PAGE 
+  output$var_desc <- renderText({
+    data_notes = switch(input$industry2,
+                        "All" = "All business establishments.",
+                        "Retail" = "Retail Trade (NAICS 44-45) and Accommodation and Food Service (NAICS 72).",
+                        "Education/Healthcare" = "Educational Services (NAICS 61) and Health Care and Social Assistance (NAICS 62).",
+                        "FIRE" = "Finance and Insurance (NAICS 52) and Real Estate and Rental and Leasing (NAICS 53).",
+                        "Professional" = "Information (NAICS 51), Professional, Scientific, and Technical Services (NAICS 54), and Management of Companies and Enterprises (NAICS 55).",
+                        "Service" = "Administrative and Support and Waste Management and Remediation Services (NAICS 56) and Other Services except Public Administration (NAICS 81).",
+                        "Industrial" = "Utilities (NAICS 22), Manufacturing (NAICS 31-33), Wholesale Trade (NAICS 42), and Transportation and Warehousing (NAICS 48-49).")
+    paste(" -- ", input$industry2, " includes ", data_notes, sep="")
+  })
+  output$within2 <- renderText(paste("Total businesses relocating WITHIN ", input$city2, ":", cats[,grep(options2$choose2, colnames(cats))][cats$NAME10==input$city2]))
+  output$total2 <- renderText(paste("Total businesses ", input$inout2, input$city2, ":", sum(cats[,grep(options2$choose2, colnames(cats))][cats$NAME10!=input$city2])))
+  output$tablelabel2 <- renderText(paste("Top 5 cities which", input$industry2, "businesses", input$inout2, input$city2, switch(input$inout2, "relocating to"="come from:", "relocating from"="go to:")))
+  output$topfive2 <- renderTable({
+    b <- cbind(as.character(cats[order(-cats[,grep(options2$choose2, colnames(cats))]),][2:6,6]),
+      as.numeric(cats[order(-cats[,grep(options2$choose2, colnames(cats))]),][2:6,grep(options2$choose2, colnames(cats))])  )
+  })
 })
 
--- ui.R
+++ ui.R
@@ -3,47 +3,57 @@ library(leaflet)
 library(sp)
 library(maptools)
 
-# Files must be read in here if ui.R calls something from them (in this case, 'names')
 sub <- readShapePoly("SoCal_place_2010_all")
 dfsub <- data.frame(sub)
 names = as.character(unique(unlist(dfsub$NAME10[dfsub$oall>9 & dfsub$iall>9 & dfsub$CLASSFP10=='C1'])))
 
-# This app uses a tab panel with a custom .css style. The whole UI is within the parentheses following div()
-shinyUI(navbarPage("Business Relocations in Southern California", id="nav",
-  tabPanel("2010-2014", div(class="outer", tags$head(includeCSS("styles.css")),
-        # Indicates where to put the leaflet (map) output, and for it to fill the page                                                       
+shinyUI(navbarPage("Business Relocations in Southern California, 2010-2014", id="nav",
+  tabPanel("Total (Map)", div(class="outer", tags$head(includeCSS("styles.css")),
         leafletOutput("myMap", width="100%", height="100%"),
-        # This is the right-side user input panel 
         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto", width = 330, height = "auto",
                      br(""),
-                     radioButtons("inout", label=strong("Select Businesses which are:"), choices=list("relocating to", "relocating from"), selected="relocating to", inline=T),
+                     radioButtons("inout", label=strong("Select Businesses which are:", style="color:blue"), choices=list("relocating to", "relocating from"), selected="relocating to", inline=T),
                      selectInput("city", label=strong("Select City:"), selected="Irvine", choices = names[order(names)]),
-                     
-                     selectInput("industry", label=strong("Select Industry: "), selected="All",
-                                 choices = list("All", "Retail", "Education/Healthcare", "FIRE", "Professional", "Service", "Industrial")),  
-                     
-                     actionButton("go", label="Click to Refresh After Changing Selection"),
+                     actionButton("go", label=a("Click to Refresh After Changing Selection"), style="color:red"),
                      br(""),
                      textOutput("within"),
                      textOutput("total"),
                      textOutput("tablelabel"),
                      tableOutput("topfive")
                      ),
-        # this is a small left-side panel with some additional info. 
         absolutePanel(id = "controls", class="panel panel-default", fixed = TRUE,
                      draggable=TRUE, top=110, left=10, right="auto", bottom="auto", width=180, height="auto",
                      p(strong("Data Notes:")),
                      h6("-- This webmap shows where businesses are moving to, or where they came from."),
                      h6("-- Choose a city and whether to display businesses that relocated to or from that city."),
-                     h6("-- Please click to refresh after making new selections to ensure correct map and legend are displayed."),
+                     h6("-- Click to refresh after making new selections to ensure correct map and legend are displayed."),
                      h6("-- Clicking on a city displays the number of businesses moving into/from the city selected on the right side panel."),
                      h6("-- A relocation is defined as a business establishment that moves at least 0.1 miles in a given year."),
                      h6("-- Please be patient while the map loads!"),
-                     #h6(textOutput("var_desc")),
                      h6("-- By the UCI", a("Metropolitan Futures Initiative.", href="http://mfi.soceco.uci.edu", target="_blank"), 
                         "See ", a("the full report here.", href="http://mfi.soceco.uci.edu/category/quarterly-report", target="_blank"))
                     )
-))))
+)),
+  tabPanel("By Industry (Tables)", div(class="outer", tags$head(includeCSS("styles.css")),
+          sidebarLayout(
+            sidebarPanel(
+              radioButtons("inout2", label=strong("Select Businesses which are:", style="color:blue"), choices=list("relocating to", "relocating from"), selected="relocating to", inline=T),
+              selectInput("city2", label=strong("Select City:"), selected="Irvine", choices = names[order(names)]),
+              selectInput("industry2", label=strong("Select Industry: "), selected="All",
+                          choices = list("All", "Retail", "Education/Healthcare", "FIRE", "Professional", "Service", "Industrial")),  
+              actionButton("go2", label=a("Click to Refresh After Changing Selection", style="color:red")),
+              br(""),
+              textOutput("var_desc")
+            ),
+            mainPanel(
+              h2(strong("Relocations by Industry", style="color:blue")),
+              p(em("This panel displays statistics for business relocations by industry, which are not able to be shown in the map panel due to spatial data limitations.", style="color:blue")),
+              textOutput("within2"),
+              textOutput("total2"),
+              textOutput("tablelabel2"),
+              tableOutput("topfive2")
+            ))))
+))
 
 
